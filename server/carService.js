e7ac4844520730c6b831ee1457db254f1d27660f
Epidemra <sergeinevgen2588@gmail.com>
2020-03-15 16:59:02 +0300

    server done

--- server/carService.js
+++ server/carService.js
@@ -1,26 +1,26 @@
-const Car  = require('../car');
+const Car  = require('./car');
 
-function getCars(data) {
-  const cars = Car.findAll();
+async function getCars(data) {
+  const cars = await Car.findAll();
   return cars;
 }
 
-function createCar(args) {
+async function createCar(args) {
   console.log(args)
-  const result = Car.create(args.carInput)
+  const result = await Car.create(args.carInput)
   console.log('result', result.dataValues);
   return result.dataValues;
 }
 
-function updateCar(args) {
+async function updateCar(args) {
   console.log(args);
   const {id, ...data} = args;
-  const car = Car.update(data, {where: { id: id }});
+  const car = await Car.update(data, {where: { id: id }});
   console.log('result', car.dataValues);
   return car.dataValues;
 }
 
-function deleteCar({id}) {
+async function deleteCar({id}) {
   const car = await Car.destroy({where: {id: id}});
   console.log(car)
   return {id : car.id};
